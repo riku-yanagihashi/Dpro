f3a88d536622929d75cca53efe4b3ea053ad782c
柳橋莉空 <yagihashirikuu@yagihashirikuunoMacBook-Air.local>
2024-10-16 10:32:44 +0900

    style: デザインの変更とログイン機能の遷移機能の変更

--- sites/accounts/urls.py
+++ sites/accounts/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
 
+app_name = 'accounts'
+
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),  # ログアウトURL
+    path('logout/', auth_views.LogoutView.as_view(next_page='accounts:login'), name='logout'),  # ログアウトURL
     path('signup/', views.signup, name='signup'),
     path('', views.index, name='index'),
 ]
