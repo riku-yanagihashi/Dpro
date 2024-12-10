8f6918ab4a3457a15ddb76f55816dbd2bcf4433d
柳橋莉空 <yagihashirikuu@yagihashirikuunoMacBook-Air.local>
2024-12-10 12:28:22 +0900

    デザインのちょこっと修正

--- sites/static/js/search_books/filter.js
+++ sites/static/js/search_books/filter.js
@@ -2,3 +2,10 @@ function toggleFilterMenu() {
     const filterMenu = document.getElementById('filter-menu');
     filterMenu.classList.toggle('open');
 }
+
+
+document.getElementById('logout-link').addEventListener('click', function (event) {
+    event.preventDefault(); // デフォルトのリンク動作を無効化
+    document.getElementById('logout-form').submit(); // フォームを送信
+});
+
